572083a6b172cd56ab173e2155e091e7afb94f87
Yash Varshney <yashvarshney@pop-os.localdomain>
2023-01-21 23:28:12 +0530

    small changes

--- example/views.py
+++ example/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
- 
+count=0
 def index(request):
-    return render(request,"example/index.html")
+    return render(request,"example/index.html",{
+        "count":count
+    })
